1e65d22b3b66dcb465b6ec7444c81f7bbd5c3574
Peco-Ti <red.snp.and@gmail.com>
2024-01-02 23:25:08 +0900

    fix

--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,13 @@ const rest = new REST({ version: '10' }).setToken(token);
     console.log('サーバーにコマンドを登録中...');
 
     await rest.put(Routes.applicationGuildCommands(clientId, guildId), {
-      body: [agentPickCommand.data, autoCompositionCommand.data, mapSelectCommand.data,memberAllocationCommand.data,diceCommand.data],
+      body: [
+        agentPickCommand.data,
+        autoCompositionCommand.data,
+        mapSelectCommand.data,
+        memberAllocationCommand.data,
+        diceCommand.data,
+      ],
     });
 
     console.log('コマンドの登録が完了しました');
@@ -41,6 +47,8 @@ const commands = {
   [agentPickCommand.data.name]: agentPickCommand,
   [autoCompositionCommand.data.name]: autoCompositionCommand,
   [mapSelectCommand.data.name]: mapSelectCommand,
+  [memberAllocationCommand.data.name]: memberAllocationCommand,
+  [diceCommand.data.name]: diceCommand,
 };
 
 // インタラクションが発生時に実行
@@ -52,13 +60,6 @@ discord.on('interactionCreate', async (interaction: Interaction) => {
     // コマンドが存在すれば実行
     if (command) {
       command.execute(interaction);
-    }
-
-    if (interaction.commandName === memberAllocationCommand.data.name) {
-      memberAllocationCommand.execute(interaction);
-    }
-    if (interaction.commandName === diceCommand.data.name) {
-      diceCommand.execute(interaction);
     }
   }
 });
