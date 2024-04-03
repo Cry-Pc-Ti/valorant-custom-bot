26c4579cd830fb5db003c0d81f3ceeea6f935ec8
Cry_Pc_Ti <red.snp.and@gmail.com>
2024-04-03 11:53:18 +0900

    fix サイコロの画像ファイルの命名をスネークケースに統一 チンチロコマンドの記法を修正・オプションを追加

--- src/events/chinchiroResult.ts
+++ src/events/chinchiroResult.ts
@@ -1,16 +1,42 @@
-
 // サイコロを振った結果を出力
-//TODO もっといい書き方ないの。
-export const chinchiroResult = async (randomIndexArray: number[]) => {
-    let chinchiroResultWords = ""
-    randomIndexArray.sort()
-    if(randomIndexArray[0] === 4 && randomIndexArray[1] === 5 && randomIndexArray[2] === 6) chinchiroResultWords = `シゴロ: ${randomIndexArray[0]} ${randomIndexArray[1]} ${randomIndexArray[2]}サイ`
-    else if(randomIndexArray[0] === 1 && randomIndexArray[1] === 1 && randomIndexArray[2] === 1) chinchiroResultWords = `ピンゾロ: ${randomIndexArray[0]} ${randomIndexArray[1]} ${randomIndexArray[2]}`
-    else if(randomIndexArray[0] === 1 && randomIndexArray[1] === 2 && randomIndexArray[2] === 3) chinchiroResultWords = `ヒフミ: ${randomIndexArray[0]} ${randomIndexArray[1]} ${randomIndexArray[2]}`
-    else if(randomIndexArray[0] === randomIndexArray[1] && randomIndexArray[1]  ===  randomIndexArray[2]) chinchiroResultWords = `アラシ: ${randomIndexArray[0]}`
-    else if(randomIndexArray[0] === randomIndexArray[1] ) chinchiroResultWords = `目: ${randomIndexArray[2]}`
-    else if(randomIndexArray[1] === randomIndexArray[2] ) chinchiroResultWords = `目: ${randomIndexArray[0]}`
-    else chinchiroResultWords = `目なし: ${randomIndexArray[0]} ${randomIndexArray[1]} ${randomIndexArray[2]}`
+export const exportChinchiroResult = async (randomIndexArray: number[]) => {
+  let result = '';
+  const isAllIncludes = (array: number[], target: number[]) => array.every((element) => target.includes(element));
+  const arrayString = `${randomIndexArray[0]}, ${randomIndexArray[1]}, ${randomIndexArray[2]}`;
 
-    return chinchiroResultWords
-}
+  switch (true) {
+    case isAllIncludes(randomIndexArray, [1, 1, 1]):
+      result = `ピンゾロ : ${arrayString}`;
+      break;
+
+    case randomIndexArray.every((num) => num === randomIndexArray[0]):
+      result = `アラシ : ${randomIndexArray[0]}`;
+      break;
+
+    case isAllIncludes(randomIndexArray, [4, 5, 6]):
+      result = `シゴロ : ${arrayString}サイ`;
+      break;
+
+    case randomIndexArray[0] === randomIndexArray[1] && randomIndexArray[1] !== randomIndexArray[2]:
+      result = `目 : ${randomIndexArray[2]}`;
+      break;
+
+    case randomIndexArray[1] === randomIndexArray[2] && randomIndexArray[2] !== randomIndexArray[0]:
+      result = `目 : ${randomIndexArray[0]}`;
+      break;
+
+    case randomIndexArray[2] === randomIndexArray[0] && randomIndexArray[0] !== randomIndexArray[1]:
+      result = `目 : ${randomIndexArray[1]}`;
+      break;
+
+    case isAllIncludes(randomIndexArray, [1, 2, 3]):
+      result = `ヒフミ : ${arrayString}`;
+      break;
+
+    default:
+      result = `役なし : ${arrayString}`;
+      break;
+  }
+
+  return result;
+};
