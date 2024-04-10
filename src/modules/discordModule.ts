3940b5f4af0d30067dc2977c04bd56408f91456b
Cry_Pc_Ti <red.snp.and@gmail.com>
2024-04-10 14:51:36 +0900

    fix コマンドをグローバルコマンドに変更 (どのサーバーでもウィングマンくん使用可)

--- src/modules/discordModule.ts
+++ src/modules/discordModule.ts
@@ -7,10 +7,9 @@ dotenv.config();
 // Discord Botのトークン
 const token = process.env.DISCORD_TOKEN as string;
 const clientId = process.env.DISCORD_CLIENT_ID as string;
-const guildId = process.env.DISCORD_GUILD_ID as string;
 const mainVoiceChannelId = process.env.DISCORD_MAIN_VOICE_CHANNEL_ID as string;
 const subVoiceChannelId = process.env.DISCORD_SUB_VOICE_CHANNEL_ID as string;
 
 const discord = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates] });
 
-export { discord, token, clientId, guildId, mainVoiceChannelId, subVoiceChannelId };
+export { discord, token, clientId, mainVoiceChannelId, subVoiceChannelId };
