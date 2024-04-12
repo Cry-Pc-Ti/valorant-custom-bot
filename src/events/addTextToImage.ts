963ab2b394336d7b8050958b9d438c061a6e19ac
Cry_Pc_Ti <red.snp.and@gmail.com>
2024-04-12 15:59:50 +0900

    add diceコマンドの結果表示形式を変更

--- src/events/addTextToImage.ts
+++ src/events/addTextToImage.ts
@@ -1,37 +1,43 @@
-import { createCanvas, loadImage, registerFont } from 'canvas';
+import { CanvasRenderingContext2D, createCanvas, loadImage, registerFont } from 'canvas';
 import fs from 'fs';
 
+// フォントを読み込む
 registerFont('./static/fonts/Yomogi-Regular.ttf', { family: 'Yomogi' });
 
-const addTextToImage = async (imagePath: string, text: string, outputPath: string): Promise<void> => {
-  const image = await loadImage(imagePath);
-  const canvas = createCanvas(image.width, image.height);
-  const context = canvas.getContext('2d');
+export const addTextToImage = async (num: number) => {
+  try {
+    // 数字によって画像パスを変更
+    let imagePath = './static/img/wingman/wingman_';
 
-  context.drawImage(image, 0, 0, image.width, image.height);
+    if (num === 100) {
+      imagePath += 'ecstatic.png';
+    } else if (num >= 90 && num <= 99) {
+      imagePath += 'joyful.png';
+    } else if (num >= 50 && num <= 89) {
+      imagePath += 'content.png';
+    } else if (num >= 10 && num <= 49) {
+      imagePath += 'neutral.png';
+    } else if (num >= 1 && num <= 9) {
+      imagePath += 'sad.png';
+    } else return;
 
-  context.fillStyle = 'black';
-  context.font = '256px Yomogi';
-  context.textAlign = 'center';
-  context.fillText(text, canvas.width / 2, canvas.height / 3.6);
+    // 画像を読み込む
+    const image = await loadImage(imagePath);
 
-  const outputStream = fs.createWriteStream(outputPath);
-  const stream = canvas.createPNGStream();
-  stream.pipe(outputStream);
+    // 画像にテキストを追加
+    const canvas = createCanvas(image.width, image.height);
+    const context: CanvasRenderingContext2D = canvas.getContext('2d');
 
-  return new Promise<void>((resolve, reject) => {
-    outputStream.on('finish', resolve);
-    outputStream.on('error', reject);
-  });
+    context.drawImage(image, 0, 0, image.width, image.height);
+    context.fillStyle = 'black';
+    context.font = '198px "Yomogi"';
+    context.textAlign = 'center';
+    context.fillText(num.toString(), canvas.width / 2, canvas.height / 4.5);
+
+    const outputStream = fs.createWriteStream('./static/img/generate_image.png');
+    const stream = canvas.createPNGStream();
+    stream.pipe(outputStream);
+  } catch (error) {
+    console.error(error);
+  }
 };
-
-const main = async () => {
-  const imagePath = './static/img/dice/wingman.png';
-  const text = '99';
-  const outputPath = 'output_image.png';
-
-  await addTextToImage(imagePath, text, outputPath);
-  console.log('Image created!');
-};
-
-main().catch(console.error);
