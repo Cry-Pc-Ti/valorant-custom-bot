3a38acd6f20238396b3ba21a666f5d40c72fb2f0
Rixu324 <ryuma.miya324@gmail.com>
2024-06-12 18:40:39 +0900

    fix がちすまんv2

--- src/events/common/readJsonData.ts
+++ src/events/common/readJsonData.ts
@@ -1,19 +1,14 @@
 import fs from 'fs';
 import { AgentData, MapData } from '../../types/valorantData';
-import { SpotifyPlaylistInfo } from '../../types/spotifyData';
 
 // JSONファイルのパス
 const valorantJsonFilePath = './static/data/valorantData.json';
-const spotifyJsonFilePath = './static/data/spotifyPlayListData.json';
 
 // JSONファイルの読み込み
 const valorantFileData = fs.readFileSync(valorantJsonFilePath, 'utf-8');
-const spotifyFileData = fs.readFileSync(spotifyJsonFilePath, 'utf-8');
 
 // JSONデータをオブジェクトに変換
 const valorantJsonData: { agents: AgentData[]; maps: MapData[] } = JSON.parse(valorantFileData);
-const spotifyJsonData: { playlist: SpotifyPlaylistInfo[] } = JSON.parse(spotifyFileData);
 
 export const valorantAgents = valorantJsonData.agents;
 export const valorantMaps = valorantJsonData.maps;
-export const spotifyPlaylistId = spotifyJsonData.playlist;
