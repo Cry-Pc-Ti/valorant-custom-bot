4d44091a2f3bbd062af6804e95f90b92a5e6a541
Rixu324 <ryuma.miya324@gmail.com>
2024-06-12 18:01:31 +0900

    fix すまん。ほんとすまん

--- src/commands/help/helpCommand.ts
+++ src/commands/help/helpCommand.ts
@@ -1,5 +1,4 @@
 import { ChatInputCommandInteraction, SlashCommandBuilder } from 'discord.js';
-import { getSpotifyPlayList } from '../../events/notion/getSpotifyPlayList';
 
 export const helpCommand = {
   data: new SlashCommandBuilder().setName('help').setDescription('何かまよったらこちらへ！').toJSON(),
@@ -7,9 +6,8 @@ export const helpCommand = {
   execute: async (interaction: ChatInputCommandInteraction) => {
     await interaction.deferReply();
 
-    getSpotifyPlayList();
-    // interaction.editReply(
-    //   `何かお困りの際はこちらをご確認ください。\nそれでも解決しない場合は記載されている連絡先にご連絡ください\nhttps://wingman-kun.notion.site/Discord-Bot-b9b2f66d841b440f9a4e466aedc5fa49`
-    // );
+    interaction.editReply(
+      `何かお困りの際はこちらをご確認ください。\nそれでも解決しない場合は記載されている連絡先にご連絡ください\nhttps://wingman-kun.notion.site/Discord-Bot-b9b2f66d841b440f9a4e466aedc5fa49`
+    );
   },
 };
